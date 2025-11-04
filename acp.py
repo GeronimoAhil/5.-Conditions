209d7f36076dda00e5cd8ab23a1ba138caa9bf57
GeronimoAhil <geromy2014@gmail.com>
2025-11-04 19:08:37 +0800

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,7 @@
+i = int(input("Which season do you like more, summer or winter?"))
+
+if (i == summer):
+    print("Your favorite season is summmer!")
+
+else: 
+    print("Your favorite season is winter!")
